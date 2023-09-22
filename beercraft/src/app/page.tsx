0e8c7f3ccf56e6fd5bfca4b482b94686479e06bb
Franciane Pires <franpp22@gmail.com>
2023-09-21 22:11:35 -0300

    prettier ajust

--- beercraft/src/app/page.tsx
+++ beercraft/src/app/page.tsx
@@ -1,21 +1,24 @@
-
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24" style={{ backgroundImage: 'url("/beer.png")', backgroundSize: "cover", backgroundPosition: "center", backgroundRepeat: "no-repeat"}}>
+    <main
+      className='flex min-h-screen flex-col items-center justify-between p-24'
+      style={{
+        backgroundImage: 'url("/beer.png")',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+      }}
+    >
       <section className='bg-dark-brown bg-opacity-50 p-24 rounded-lg flex flex-col justify-center items-center gap-14 py-auto'>
-      <h2 className="text-4xl font-semibold">Are you a craft beer enthusiast?</h2>
-      <p className="text-xl font-semibold">Looking for an easy way to manage your collection of beer-related products?</p>
-      <p className="text-xl font-semibold">Want to explore a wide variety of beers from around the world?</p>
-      <p className="text-xl font-semibold">If so, you're in the right place!</p>
+        <h2 className='text-4xl font-semibold'>Are you a craft beer enthusiast?</h2>
+        <p className='text-xl font-semibold'>
+          Looking for an easy way to manage your collection of beer-related products?
+        </p>
+        <p className='text-xl font-semibold'>
+          Want to explore a wide variety of beers from around the world?
+        </p>
+        <p className='text-xl font-semibold'>If so, you're in the right place!</p>
       </section>
     </main>
-
-  )
+  );
 }
-
-
-
-   
-
-  
